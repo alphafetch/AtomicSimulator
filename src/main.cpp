bb59de74bce522cf4619ef6dfdec0621c02c5f04
theTacocat88 <eliasglick1@gmail.com>
2026-09-23 16:49:46 -0400

    feat: add protein label to molecule display in atom simulation

--- src/main.cpp
+++ src/main.cpp
@@ -282,6 +282,7 @@ int main() {
                 } else if (!molecule.labelDrawn) {
                     std::string form = molecule.formula;
                     std::string label = "[ " + form + "]";
+                    if (molecule.isProtein) label += " - Prtn.";
                     const char* moleculeLabel = label.c_str();
                     molecule.labelDrawn = true;
                     drawCenteredLabel(molecule.centeroid.x, molecule.centeroid.y, moleculeLabel, settings, RELATIVE_TEXT_HEIGHT_MOLECULE, Fade(GRAY, 0.8f));
