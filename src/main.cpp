77723acec62a485585e6a81844b4aeea34d394ab
theTacocat88 <eliasglick1@gmail.com>
2026-09-24 19:10:18 -0400

    fix: remove unused includes in main.cpp to streamline code

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,12 @@
-#include <random>
-#include <fstream>
-#include <iostream>
-#include <string>
 #include <cstdlib>
-#include <numbers>
-#include <vector>
-#include <array>
-#include <cmath>
-#include <chrono>
 
 #include "raylib.h"
 #define RAYGUI_IMPLEMENTATION
 #include "../external/raygui.h"
 
 #include "../core/constants.hpp"
-#include "../core/utils.hpp"
-#include "../core/vector.hpp"
-#include "../core/elements.hpp"
-#include "../core/settings.hpp"
 
 #include "../sim/screens.hpp"
-#include "../sim/atom.hpp"
-#include "../sim/utils.hpp"
-#include "../sim/bondFind.hpp"
-#include "../sim/molecule.hpp"
-#include "../sim/render.hpp"
-#include "../sim/proteinBuilder.hpp"
-
-using std::cout;
 
 int main() {
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, MAIN_WINDOW_TITLE);
